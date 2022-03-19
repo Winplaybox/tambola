834cb912534948a5c39cf11122771a18535040ba
winplaybox <sagarsuthar530@gmail.com>
2022-03-19 15:39:44 +0530

    'updateshot'

--- frontend/src/components/utils/settings.tsx
+++ frontend/src/components/utils/settings.tsx
@@ -1,5 +1,16 @@
-export default {
+import _ from 'lodash';
 
+export default {
+    availableSpeakersList: [
+        {
+            title: 'Indian English - Aditi (FEMALE)',
+            path: 'aditi',
+        },
+        {
+            title: 'US English - Matthew (MALE)',
+            path: 'matthew',
+        }
+    ],
     reactions: {
         lol: { key: 'lol', name: 'LOL', imgName: 'react-lol', className: 'lol' },
         enjoy: { key: 'enjoy', name: 'Enjoy', imgName: 'react-enjoy', className: 'enjoy' },
@@ -13,14 +24,28 @@ export default {
     botts: (val:any)=>`https://avatars.dicebear.com/api/bottts/${val}.svg`,
     shartext: (val:any)=>`Use this invite: ${val}`,
     awards: {
-        corner: { key: 'corner', name: 'Corners', count: '2', pts: '30' },
-        earlyfive: { key: 'earlyfive', name: 'Early Five', count: '1', pts: '40' },
-        earlyseven: { key: 'earlyseven', name: 'Early Seven', count: '1', pts: '30' },
-        firstline: { key: 'firstline', name: 'First Line', count: '1', pts: '20' },
-        house: { key: 'house', name: 'Full house', count: '2', pts: '70' },
-        lastline: { key: 'lastline', name: 'Last Line', count: '1', pts: '20' },
-        middleline: { key: 'middleline', name: 'Middle Line', count: '1', pts: '20' },
-        middlenumber: { key: 'middlenumber', name: 'Middle Number', count: '1', pts: '30' },
-        star: { key: 'star', name: 'Star', count: '2', pts: '30' },
-    }
+        corner: { key: 'corner', name: 'Corners', count: '2', pts: '30',status:'1' },
+        earlyfive: { key: 'early-five', name: 'Early Five', count: '1', pts: '40',status:'1' },
+        earlyseven: { key: 'early-seven', name: 'Early Seven', count: '1', pts: '30',status:'1' },
+        firstline: { key: 'first-line', name: 'First Line', count: '1', pts: '20',status:'1' },
+        house: { key: 'house', name: 'Full house', count: '2', pts: '70',status:'1' },
+        lastline: { key: 'last-line', name: 'Last Line', count: '1', pts: '20',status:'1' },
+        middleline: { key: 'middle-line', name: 'Middle Line', count: '1', pts: '20',status:'1' },
+        middlenumber: { key: 'middle-number', name: 'Middle Number', count: '1', pts: '30',status:'1' },
+        star: { key: 'star', name: 'Star', count: '2', pts: '30',status:'1' },
+    },
+    groupArrBy: (arr: any[], key: string | number, flatten = true) => {
+        var result = arr.reduce(function (groups: { [x: string]: any[]; }, item: { [x: string]: any; }) {
+            const val = item[key];
+            groups[val] = groups[val] || [];
+            groups[val].push(item);
+            return groups;
+        }, {});
+
+        let returnArr = Object.keys(result).map(function (key) {
+            return result[key];
+        });
+
+        return flatten ? _.flatten(returnArr) : returnArr;
+    },
 }
